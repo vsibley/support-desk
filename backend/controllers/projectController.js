0b191900dc59ffb7ff126a59ec733d78672be3e8
Vanessa Sibley <sibley.vanessas@gmail.com>
2022-11-05 17:52:50 -0700

    added update and delete functions and routes

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -19,6 +19,30 @@ const getProjects = asyncHandler(async (req, res) => {
   res.status(200).json(projects)
 });
 
+// @desc  Get users project
+// @route  GET/api/projects/:id
+// @access  Private
+const getSingleProject = asyncHandler(async (req, res) => {
+  //Get user using ID&JWT
+  const user = await User.findById(req.user.id)
+
+  if(!user) {
+    res.staus(401)
+    throw new Error('User not found')
+  }
+  const project = await Project.findById(req.params.id)
+
+  if(!project) {
+    res.status(404)
+    throw new Error('Project Not found')
+  }
+  if(project.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not Authorized')
+  }
+
+  res.status(200).json(project)
+});
 
 
 // @desc  Get users projects
@@ -49,7 +73,65 @@ const createProject = asyncHandler(async (req, res) => {
   res.status(201).json(project);
 });
 
+// @desc  Delete a ticket
+// @route  DELETE/api/projects/:id
+// @access  Private
+const deleteProject = asyncHandler(async (req, res) => {
+  //Get user using ID&JWT
+  const user = await User.findById(req.user.id)
+
+  if(!user) {
+    res.staus(401)
+    throw new Error('User not found')
+  }
+  const project = await Project.findById(req.params.id)
+
+  if(!project) {
+    res.status(404)
+    throw new Error('Project Not found')
+  }
+  if(project.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not Authorized')
+  }
+  await project.remove()
+
+  res.status(200).json({success: true})
+})
+
+// @desc  update project
+// @route  PUT/api/projects/:id
+// @access  Private
+const updateProject = asyncHandler(async (req, res) => {
+  //Get user using ID&JWT
+  const user = await User.findById(req.user.id)
+
+  if(!user) {
+    res.staus(401)
+    throw new Error('User not found')
+  }
+  const project = await Project.findById(req.params.id)
+
+  if(!project) {
+    res.status(404)
+    throw new Error('Project Not found')
+  }
+  if(project.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not Authorized')
+  }
+
+  const updatedProject = await Project.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+  res.status(200).json(updatedProject)
+});
+
+
+
 module.exports = {
     getProjects,
-    createProject
+    createProject,
+    getSingleProject, 
+    deleteProject, 
+    updateProject
 }
--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getProjects, createProject } = require("../controllers/projectController");
+const { getProjects, createProject, getSingleProject, updateProject, deleteProject } = require("../controllers/projectController");
 
 const { protected } = require("../middleware/authMiddleware");
 
-router.route("/").get(protected, getProjects).post(protected, createProject);
+router.route("/").get(protected, getProjects).post(protected, createProject)
+
+router.route('/:id').get(protected, getSingleProject).delete(protected, deleteProject).put(protected, updateProject)
 
 module.exports = router;
