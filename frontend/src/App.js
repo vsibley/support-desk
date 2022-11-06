0d8282918c0fdc75ccb5b0a82e2a5d42aa8d1508
Vanessa Sibley <sibley.vanessas@gmail.com>
2022-11-05 18:07:35 -0700

    Created custom hook for private route and nested routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,10 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css";
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute';
 import Home from './pages/Home'
 import Login from './pages/Login'
+import NewProject from './pages/NewProject';
 import Register from './pages/Register'
 
 
@@ -17,6 +19,9 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login/>} />
           <Route path='/register' element={<Register/>} />
+          <Route path='/new-project' element={<PrivateRoute />}> 
+           <Route path='/new-project' element={<NewProject />} />
+          </Route>
         </Routes>
       </div>
     </Router>
---
+++ frontend/src/pages/NewProject.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NewProject() {
+  return (
+    <div>NewTicket</div>
+  )
+}
+
+export default NewProject
