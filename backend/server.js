215e51d0dbb8a399b2fcf25028be19e3fee3bc07
Vanessa Sibley <sibley.vanessas@gmail.com>
2022-11-11 07:25:04 -0800

    heroku post build script -- prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path =('path')
 const colors = require("colors");
 const dotenv = require("dotenv").config()
 const connectDB = require('./config/db')
@@ -21,6 +22,18 @@ app.get('/', (req, res) => {
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/projects', require('./routes/projectRoutes'))
 
+//Serve the frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
+} else {
+  app.get("/", (_, res) => {
+    res.status(200).json({ message: "Welcome to the Social Support API" });
+  });
+}
 
 app.use(errorHandler)
 
